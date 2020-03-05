626c7f360bfdc42561dfa53a29b0a599c4a126d8
Anna Iashina <95schatt@gmail.com>
2020-03-05 15:31:34 +0200

    two sigma graphs are rendered

--- public/js/public.js
+++ public/js/public.js
@@ -1,8 +1,132 @@
+var left, right;
+
+function refreshGraph(name, side) {
+    sigma.classes.graph.addMethod('neighbors', function (nodeId) {
+        var k,
+            neighbors = {},
+            index = this.allNeighborsIndex[nodeId] || {};
+
+        for (k in index)
+            neighbors[k] = this.nodesIndex[k];
+
+        return neighbors;
+    });
+    sigma.parsers.json(name, {
+        container: side + "_svg",
+        render: {
+            container: document.getElementById(side + "_svg"),
+            type: 'canvas'
+        },
+        settings: {
+            minNodeSize: 8,
+            maxNodeSize: 16
+        }
+    
+    },
+        function (s) {
+            console.log(s)
+
+            s.graph.nodes().forEach(function (node, i, a) {
+                node.x = Math.cos(Math.PI * 2 * i / a.length);
+                node.y = Math.sin(Math.PI * 2 * i / a.length);
+            });
+            s.refresh()
+            sigma.plugins.relativeSize(s, 2);
+            s.graph.nodes().forEach(function (n) {
+                n.originalColor = n.color;
+                console.log(n);
+            });
+            s.graph.edges().forEach(function (e) {
+                e.originalColor = e.color;
+            });
+            s.bind('clickNode', function (e) {
+                var nodeId = e.data.node.id,
+                    toKeep = s.graph.neighbors(nodeId);
+                toKeep[nodeId] = e.data.node;
+
+                s.graph.nodes().forEach(function (n) {
+                    if (toKeep[n.id]) {
+                        n.color = n.originalColor;
+                    }
+                    else {
+                        n.color = '#eee';
+                    }
+                });
+
+                s.graph.edges().forEach(function (e) {
+                    if (toKeep[e.source] && toKeep[e.target]) {
+                        e.color = e.originalColor;
+                    }
+                    else {
+                        e.color = '#eee';
+                    }
+                });
+
+                s.refresh();
+            });
+
+            s.bind('clickStage', function (e) {
+                s.graph.nodes().forEach(function (n) {
+                    n.color = n.originalColor;
+                    console.log(n.originalColor);
+                });
+
+                s.graph.edges().forEach(function (e) {
+                    e.color = e.originalColor;
+                });
+
+                // Same as in the previous event:
+                s.refresh();
+            });
+
+            atlasObj = s.startForceAtlas2({
+                linLogMode: true,
+                outboundAttractionDistribution: true,
+                adjustSizes: false,
+                edgeWeightInfluence: 0,
+                scalingRatio: 0.5,
+                strongGravityMode: false,
+                gravity: 2,
+                barnesHutOptimize: false,
+                barnesHutTheta: 0.5,
+                slowDown: 1,
+                startingIterations: 1,
+                iterationsPerRender: 1,
+                worker: true
+            });
+            window.setTimeout(function () { s.stopForceAtlas2(); }, 5000);
+            var dragListener = sigma.plugins.dragNodes(s, s.renderers[0]);
+
+            dragListener.bind('startdrag', function (event) {
+                s.stopForceAtlas2();
+            });
+            dragListener.bind('drag', function (event) {
+                console.log(event);
+            });
+            dragListener.bind('drop', function (event) {
+                console.log(event);
+            });
+            dragListener.bind('dragend', function (event) {
+                nodes = atlasObj.supervisor.graph.nodes();
+                for (let j = 0, i = 0, l = atlasObj.supervisor.nodesByteArray.length; i < l; i += atlasObj.supervisor.ppn) {
+                    if (nodes[j] === event.data.node) {
+                        atlasObj.supervisor.nodesByteArray[i] = event.data.node.x;
+                        atlasObj.supervisor.nodesByteArray[i + 1] = event.data.node.y;
+                    }
+                    j++;
+                }
+                s.startForceAtlas2();
+                window.setTimeout(function () { s.stopForceAtlas2(); }, 5000);
+            });
+
+        } 
+    );
+}
 
 
 // load the data
 
-function network_from_selected_data(sel) {
+function network_from_selected_data(sel, side) {
     if (sel == 1) {
         var name = "data/before.json";
     } else if (sel == 2) {
@@ -15,23 +139,7 @@ function network_from_selected_data(sel) {
         var name = "data/sigma-data.json"
     }
 
-    sigma.parsers.json(name,
-        { container: window.side + '_svg',
-          settings: {defaultNodeColor: '#ec5148'}
-        },
-        function (s) { //This function is passed an instance of Sigma s
-
-            //Initialize nodes as a circle
-            s.graph.nodes().forEach(function (node, i, a) {
-                node.x = Math.cos(Math.PI * 2 * i / a.length);
-                node.y = Math.sin(Math.PI * 2 * i / a.length);
-                console.log(node.id, node.x, node.y);
-            });
-
-            //Call refresh to render the new graph
-            s.startForceAtlas2({ worker: true, barnesHutOptimize: false, startingIterations: 6, iterationsPerRender: 0});
-        });
-
+    refreshGraph(name, side);
 }
 
 
@@ -41,7 +149,7 @@ function select_data(index, side) {
     var x = document.getElementById(side + "_select")
     window.side = side;
     var selection = x.options[index].index;
-    network_from_selected_data(selection);
+    network_from_selected_data(selection, side);
 }
 
 //////////// UPLOADING A CUSTOM FILE //////////////
