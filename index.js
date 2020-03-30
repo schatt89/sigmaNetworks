6d5d65c3872cbc3270090f3aad7633b801a7f330
Anna Iashina <95schatt@gmail.com>
2020-03-30 17:46:12 +0300

    memory bug fixed

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const PORT = process.env.PORT || 7000;
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
 app.use(express.static('public/'));
-app.use(express.json('100mb'));
+//app.use(express.json('100mb'));
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
 app.post('/api', (request, response) => {
     console.log("I got a request!");
